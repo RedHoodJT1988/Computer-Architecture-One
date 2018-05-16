6dce619fd5c7e1c3edc0f9079fbbe8306f771ab2
Jonathan Reeves <jonathanedwardreeves1988@gmail.com>
2018-05-16 12:18:39 -0500

    MUL, PRN, and beautify tick function

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -1,16 +1,44 @@
 /**
  * LS-8 v2.0 emulator skeleton code
  */
-
-const LDI = 0b10011001;
-const PRN = 0b01000011;
-const HLT = 0b00000001;
 const MUL = 0b10101010;
+const ADD = 0b10101000;
+const LDI = 0b10011001;
 const POP = 0b01001100;
 const PUSH = 0b01001101;
 const CALL = 0b01001000;
+const JMP = 0b01010000;
 const RET = 0b00001001;
-const ADD = 0b10101000;
+const IRET = 0b00001011;
+const PRN = 0b01000011;
+const PRA = 0b01000010;
+const ST = 0b10011010;
+const CMP = 0b10100000;
+const JEQ = 0b01010001;
+const JNE = 0b01010010;
+const HLT = 0b00000001;
+
+// Registers
+const IM = 5;
+const IS = 6;
+const SP = 7;
+
+// Flag values
+const FL_L = 0x1 << 2;
+const FL_G = 0x1 << 1;
+const FL_E = 0x1 << 0;
+
+// Interrupt masks
+const intMask = [
+  0x1 << 0,
+  0x1 << 1,
+  0x1 << 2,
+  0x1 << 3,
+  0x1 << 4,
+  0x1 << 5,
+  0x1 << 6,
+  0x1 << 7,
+];
 
 /**
  * Class for simulating a simple Computer (CPU & memory)
@@ -26,7 +54,10 @@ class CPU {
 
     // Special-purpose registers
     this.reg.PC = 0; // Program Counter
-    this.flag = false;
+    this.PCflag = false;
+    this.reg.FL = 0b00000000;
+    this.reg[SP] = 0xf4;
+    this.ITenabled = true;
   }
 
   /**
@@ -82,8 +113,38 @@ class CPU {
     // from the memory address pointed to by the PC. (I.e. the PC holds the
     // index into memory of the instruction that's about to be executed
     // right now.)
-    const IR = this.ram.read(this.reg.PC);
+    const _push = value => {
+      this.reg[SP]--;
+
+      this.ram.write(this.reg[SP], value);
+    };
+
+    const _pop = () => {
+      const popped = this.ram.read(this.reg[SP]);
+      this.reg[SP]++;
+      return popped;
+    };
+
+    if (this.ITenabled) {
+      let maskedInterrupts = this.reg[IS] & this.reg[IM];
+      for (let i = 0; i <= 7; i++) {
+        if (((maskedInterrupts >> i) & 0b1) === 1) {
+          this.ITenabled = false;
+          this.reg[IS] &= ~intMask[i];
+          _push(this.reg.PC);
+          _push(this.reg.FL);
+          for (let j = 0; j <= 6; j++) {
+            _push(this.reg[j]);
+          }
+          this.reg.PC = this.ram.read(0xf8 + i);
+          this.PCflag = true;
+          break;
+        }
+      }
+    }
     // !!! IMPLEMENT ME
+
+    let IR = this.reg.PC;
     // Debugging output
     //console.log(`${this.PC}: ${IR.toString(2)}`);
     // Get the two bytes in memory _after_ the PC in case the instruction
@@ -95,59 +156,88 @@ class CPU {
     // outlined in the LS-8 spec.
     // !!! IMPLEMENT ME
 
-    const _push = value => {
-      this.reg[7]--;
-      this.ram.write(this.reg[7], value);
-    };
-
-    switch (IR) {
+    switch (this.ram.read(IR)) {
       case LDI:
         this.reg[operandA] = operandB;
-        break;
-      case PRN:
-        console.log(this.reg[operandA]);
         break;
       case MUL:
         this.alu('MUL', operandA, operandB);
         break;
-      case PUSH:
-        if (this.reg[7] === 0) this.reg[7] = 0xf4;
-        _push(this.reg[operandA]);
-        break;
-      case POP:
-        this.reg[operandA] = this.ram.read(this.reg[7]);
-        this.reg[7]++;
-        break;
       case ADD:
         this.alu('ADD', operandA, operandB);
         break;
+      case PUSH:
+        _push(this.reg[operandA]);
+        break;
+      case POP:
+        this.reg[operandA] = _pop();
+        break;
       case CALL:
-        this.flag = true;
+        this.PCflag = true;
         _push(this.reg.PC + 2);
         this.reg.PC = this.reg[operandA];
         break;
+      case JMP:
+        this.PCflag = true;
+        this.reg.PC = this.reg[operandA];
+        break;
       case RET:
-        this.flag = true;
-        this.reg.PC = this.ram.read(this.reg[7]);
-        this.reg[7]++;
+        this.PCflag = true;
+        this.reg.PC = this.ram.read(this.reg[SP]);
+        this.reg[SP]++;
+        break;
+      case IRET:
+        this.ITenabled = true;
+        this.PCflag = true;
+        for (let i = 6; i >= 0; i--) {
+          this.reg[i] = _pop();
+        }
+        this.reg.FL = _pop();
+        this.reg.PC = _pop();
+        break;
+      case PRN:
+        console.log(this.reg[operandA]);
+        break;
+      case PRA:
+        console.log(String.fromCharCode(this.reg[operandA]));
+        break;
+      case CMP:
+        if (this.reg[operandA] === this.reg[operandB]) this.reg.FL |= FL_E;
+        else this.reg.FL &= ~FL_L;
+        if (this.reg[operandA] > this.reg[operandB]) this.reg.FL |= FL_L;
+        else this.reg.FL &= ~FL_L;
+        if (this.reg[operandA] > this.reg[operandB]) this.reg.FL |= FL_G;
+        else this.reg.FL &= ~FL_G;
+        break;
+      case JEQ:
+        if ((this.reg.FL &= 0b00000001) === 0b1) {
+          this.PCflag = true;
+          this.reg.PC = this.reg[operandA];
+        }
+        break;
+      case JNE:
+        if ((this.reg.FL &= 0b00000001) === 0b0) {
+          this.PCflag = true;
+          this.reg.PC = this.reg[operandA];
+        }
+        break;
+      case ST:
+        this.ram.write(this.reg[operandA], this.reg[operandB]);
         break;
       case HLT:
         this.stopClock();
         break;
-      default:
-        // console.log('ERROR: Unknown instruction');
-        break;
     }
+
     // Increment the PC register to go to the next instruction. Instructions
     // can be 1, 2, or 3 bytes long. Hint: the high 2 bits of the
     // instruction byte tells you how many bytes follow the instruction byte
     // for any particular instruction.
     // !!! IMPLEMENT ME
 
-    if (!this.flag) {
-      let operandCount = (IR >>> 6) & 0b11;
-      let totalInstructionLen = operandCount + 1;
-      this.reg.PC += totalInstructionLen;
+    if (!this.PCflag) {
+      this.reg.PC++;
+      this.reg.PC += this.ram.read(IR) >> 6;
     }
     this.flag = false;
   }
